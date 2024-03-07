77b8dfa450b2065aa1bc2e996c6710d74697ba0f
Iuri Guilherme Bogar Portilho <iuribogar@hotmail.com>
2024-03-07 14:58:39 -0300

    resolução do exercício, implementação do formulário que verifica se A > B, com validação dos campos

---
+++ main.js
@@ -0,0 +1,59 @@
+let a;
+let b;
+let form = document.getElementById("formulario");
+let mensagem = document.getElementById("mensagem-retorno");
+let campoA = document.getElementById("numero-a");
+let campoB = document.getElementById("numero-b");
+let botao = document.getElementById("botao-submit");
+
+function isNumber(n) {
+    return typeof n === "number"
+}
+
+form.addEventListener("submit", function(e){
+    e.preventDefault();
+})
+
+campoA.addEventListener("keyup", function(){
+    a = campoA.value;
+    b = campoB.value;
+    mensagem.classList.remove("valid");
+    mensagem.classList.remove("invalid");
+    mensagem.innerHTML = "";
+    if (a == "" || b == "")
+    {
+        botao.disabled = true;
+    }
+    else {
+        botao.disabled = false;
+    }
+})
+
+campoB.addEventListener("keyup", function(){
+    a = campoA.value;
+    b = campoB.value;
+    mensagem.classList.remove("valid");
+    mensagem.classList.remove("invalid");
+    mensagem.innerHTML = "";
+    if (a == "" || b == "")
+    {
+        botao.disabled = true;
+    }
+    else {
+        botao.disabled = false;
+    }
+})
+
+botao.addEventListener("click", function() {
+    a = parseInt(a);
+    b = parseInt(b);
+    if (a > b) {
+        mensagem.innerHTML = `Formulário inválido, o número A (${a}) deve ser menor que o número B (${b})`;
+        mensagem.classList.add("invalid");
+    }
+    else {
+        mensagem.innerHTML = `Parabéns, formulário submetido com sucesso, o número A (${a}) é  menor que o número B (${b})`;
+        mensagem.classList.add("valid");
+    }
+})
+
