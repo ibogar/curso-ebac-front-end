99085f36267c1a81bed6f4f319d4320b6c4a112f
Iuri Guilherme Bogar Portilho <iuribogar@hotmail.com>
2024-03-13 18:18:32 -0300

    correção de comportamento inesperado ao adicionar mais de um elemento na lista

--- main.js
+++ main.js
@@ -1,22 +1,23 @@
 $(document).ready(function() {
 
+    $(".list").on("click", ".list-item", function() {
+        if ($(this).hasClass("checked-item")) {
+            $(this).removeClass("checked-item");
+        }
+        else {
+            $(this).addClass("checked-item");
+        }
+        
+    })
+
     $("form").on("submit", function(e){
         e.preventDefault();
         let task = $("#task").val();
         let listItem = `<li class="list-item"> ${task} </li>`;
         $(listItem).appendTo("ul");
         $("#task").val("");
-    
-        $(".list-item").on("click", function() {
-            if ($(this).hasClass("checked-item")) {
-                $(this).removeClass("checked-item");
-            }
-            else {
-                $(this).addClass("checked-item");
-            }
-            
-        })
     })
 
+    
 
 })
