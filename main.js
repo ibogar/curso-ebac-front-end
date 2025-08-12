2fbefb4e3d4a36ec4df3f2717e45d897c3d92026
Iuri Guilherme Bogar Portilho <iuribogar@hotmail.com>
2025-08-12 14:45:23 -0300

    resolução do exercício

---
+++ main.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function multiplicar(a, b) {
+    return a * b;
+}
+console.log(multiplicar(2, 3));
+function saudacao(nome) {
+    return "Olá " + nome;
+}
+console.log(saudacao("Iuri"));
+//# sourceMappingURL=main.js.map
---
+++ main.ts
@@ -0,0 +1,11 @@
+function multiplicar(a: number, b: number): number {
+    return a * b;
+}
+
+console.log(multiplicar(2, 3));
+
+function saudacao(nome: string): string {
+    return "Olá " + nome;
+}
+
+console.log(saudacao("Iuri"));
